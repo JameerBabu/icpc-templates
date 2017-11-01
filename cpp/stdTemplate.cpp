0248e0043338db18e43f4b36e41a62764ad4b089
Nithesh <nithusha21@yahoo.co.in>
2017-11-01 22:44:15 +0530

    Added standard template

---
+++ cpp/stdTemplate.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef pair<int, int> pii;
+typedef vector<vi> vvi;
+
+#define MAX (ll)(1e9+7)
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t = 0;
+	cin>>t;
+	while(t--){
+
+	}
+	return 0;
+}
